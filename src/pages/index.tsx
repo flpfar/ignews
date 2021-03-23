7c544685676d60f1cef4575751bc43e2b2ed5acc
Felipe Augusto Rosa <flp.far@gmail.com>
2021-03-23 08:28:19 -0300

    Add SubscribeButton component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import Head from 'next/head';
-import styles from './home.module.scss';
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return(
+  return (
     <>
       <Head>
         <title>Home | ig.news</title>
@@ -10,14 +11,17 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
-            Get access to all the publications <br/>
+            Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
-        </section>  
-        <img src="/images/avatar.svg" alt="Girl coding"/>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
-  )
+  );
 }
